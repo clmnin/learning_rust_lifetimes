d4ba04c533e1cec5078dd5cd06cd6c56007b9b76
Clament <clament.john.k@gmail.com>
2020-11-03 08:08:58 +0530

    [Rust 101] MutableIterator done
    	We'll get an error inside the next function inside the impl
    	for the unspecified lifetime of the element. We solved this
    	using stf::mem::replace which gives you the src with it's
    	original lifetime (which is 'iter which in turn is the lifetime
    	of the collection)
    	An explanation has been given in the code line:46..65

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,47 @@ impl<'a, T> Iterator for MyIterator<'a, T> {
     }
 }
 
+// here we make a mutable iterator
+// Note: mut (mutable dunder) gives an exclusive mut. No one else should have
+//       a reference to this data when one already has a mut reference (& mut)
+struct MyMutableIterator<'iter, T> {
+    slice: &'iter mut [T],
+}
+
+impl <'iter, T> Iterator for MyMutableIterator<'iter, T> {
+    type Item = &'iter mut T;
+
+    // allreferences have lifetime, so we have to define them explicitly
+    // we're borrowning self for how ever long fn next lasts for.
+    fn next<'next>(&'next mut self) -> Option<Self::Item> {
+        let slice = &mut self.slice;
+        let slice = std::mem::replace(slice, &mut []);
+        let (first, rest) = slice.split_first_mut()?;
+        self.slice = rest;
+        Some(first)
+
+        // this is how our first implementation was. We were getting a mut
+        // get_mut of the slice's element at 0. But element being inside fn next
+        // with a lifetime 'next will have a lifetime 'next
+        // and so we're getting a mut from slice of lifetime 'iter to var 
+        // element with lifetime 'next. And 'next next is a lifetime much lower
+        // than 'iter and so we are returning element with lifetime 'next.
+        // 
+        // And so we were taking in a colletion with lifetime 'a, then passing a
+        // mut reference of this collection to a struct (MyMutableIter) that
+        // takes the same lifetime as the collection (we acheived this by using 
+        // generic 'iter lifetime). but then for each yield we're retuning a
+        // different lifetime 'next
+        // So this was the reason we were having an error for the below code
+        // // get the first element
+        // let element = self.slice.get_mut(0);
+        // // set self.slice to rest of the list
+        // self.slice = &mut self.slice[1..];
+        // // return first element
+        // element
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,5 +79,16 @@ mod tests {
         for (index ,elem) in wrapper.enumerate() {
             assert_eq!(*elem, collection[index]);
         }
+
+        let mut collection: Vec<i32> = vec![1,2,3,4];
+        let wrapper = MyMutableIterator {
+            slice: &mut collection[..],
+        };
+
+        for (_index ,elem) in wrapper.enumerate() {
+            *elem = *elem + 1;
+        }
+
+        assert_eq!(collection.get(0), Some(&2));
     }
 }
